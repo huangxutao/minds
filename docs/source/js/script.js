5ad8106fd3efea954ea94712f7fca8353f784b9c
huangxutao <h784124804@gmail.com>
2017-04-10 16:44:49 +0800

    粒子动起来^_^

--- docs/source/js/script.js
+++ docs/source/js/script.js
@@ -1,94 +1,110 @@
 ;(function() {
-  /**
-   * 随机产生数据
-   * 
-   * @param {Number} num 
-   * @returns 
-   */
-  function createData(num) {
-    var data = [];
+  // 粒子相关
+  var MAX_RADIUS = 18,
+      NUM = 25,
+      particle = null,
+      particles = [];
 
-    for(var i = 0; i < num; i++) {
-      data.push({
-        x: parseInt(Math.random() * 1024, 10),
-        y: parseInt(Math.random() * 860, 10),
-        r: parseInt(Math.random() * 20, 10)
-      });
+  // 粒子的原型对象
+  var ball = {
+    color: 'rgba(255, 255, 255, 0.36)',
+    radius: 20,
+    setColor: function (color) {
+      this.color = color;
+    },
+    setRadius: function (radius) {
+      this.radius = radius;
+    },
+    draw: function (ctx) {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      ctx.fill();
+      ctx.closePath();
     }
+  };
 
-    return data;
-  }
-  /**
-   * 获取两点间距
-   * 
-   * @param {Object} pointA 
-   * @param {Object} pointB 
-   * @returns 
-   */
-  function getDistance(pointA, pointB) {
-    var x = pointA.x - pointB.x;
-    var y = pointA.y - pointB.y;
+  // 画布相关
+  var canvas = document.querySelector('#canvas'),
+      canvasWidth = canvas.width,
+      canvasHeight = canvas.height,
+      ctx = canvas.getContext('2d');
+  
+  // 数学函数、常量 PI
+  var PI = Math.PI,
+      random = Math.random,
+      floor = Math.floor,
+      sin = Math.sin,
+      cos = Math.cos;
 
-    return Math.sqrt(x*x + y*y);
-  }
-
-  /**
-   * 绘制圆
-   * 
-   * @param {Object} ctx 绘图上下文
-   * @param {Object} circle
-   */
-  function drawCircle(ctx, circle) {
-    ctx.beginPath();
-    ctx.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'rgba(255, 255, 255, .36)';
-    ctx.fill();
+  // 随机出所有粒子
+  for(var i = 0; i < NUM; i++) {
+    particle = Object.create(ball);
+    particle.x = +(random() * canvasWidth).toFixed(2);
+    particle.y = +(random() * canvasHeight).toFixed(2);
+    particle.radius = +(random() * MAX_RADIUS).toFixed(2);
+    particle.angle = +(random() * 360).toFixed(2);
+    particles.push(particle);
   }
 
-  /**
-   * 两点连线
-   * 
-   * @param {object} ctx 绘图上下文
-   * @param {Number} limt
-   * @param {Object} pointA 
-   * @param {Object} pointB 
-   * @returns 
-   */
-  function drawLline(ctx, limt, pointA, pointB) {
-    var distance = getDistance(pointA, pointB);
-
+  // 粒子间距小于一定距离 连线
+  function connect(ctx, pointA, pointB, limt) {
+    var x = pointA.x - pointB.x,
+        y = pointA.y - pointB.y,
+        distance = Math.sqrt(x*x + y*y);
+    
     if(distance > limt) {
-      return false;
+      return ;
     }
 
     ctx.beginPath();
-    ctx.lineWidth = 0.36;
+    ctx.lineWidth = 0.6;
     ctx.moveTo(pointA.x, pointA.y);
     ctx.lineTo(pointB.x, pointB.y);
-    ctx.strokeStyle = 'rgba(255, 255, 255, 0.36)';
+    ctx.strokeStyle = 'rgba(255, 255, 255, '+ (1 - distance/limt) +')';
     ctx.stroke();
+    ctx.closePath();
   }
 
-  /**
-   * 初始化
-   * 
-   */
-  function init() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var circles = createData(40);
+  // 开始绘画
+  function run() {
+    var speed = 1;
 
-    for(var i = 0, len = circles.length; i < len; i++) {
+    window.requestAnimationFrame(run);
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    for(var i = 0, len = particles.length; i < len; i++) {
       var j = i + 1;
 
       while(j < len) {
-        drawLline(ctx, 60, circles[i], circles[j]);
+        connect(ctx, particles[i], particles[j], 120);
         j++;
       }
-
-      drawCircle(ctx, circles[i]);
     }
+
+    particles.forEach(function (particle) {
+      var thisRadius = particle.radius,
+          thisAngel;
+
+      var x = particle.x;
+      var y = particle.y;
+
+      if(x > canvasWidth + 2 * MAX_RADIUS || x < -2 * MAX_RADIUS) {
+        particle.angle += 90;
+      }
+
+      if(y > canvasHeight + 2 * MAX_RADIUS || y < -2 * MAX_RADIUS) {
+        particle.angle *= -1;
+      }
+
+      thisAngle = particle.angle;
+
+      particle.x += cos(thisAngle * PI / 180) * speed;
+      particle.y += sin(thisAngle * PI / 180) * speed;
+      particle.draw(ctx);
+    });
   }
 
-  init();
+  run();
 }())
